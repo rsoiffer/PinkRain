9c0d57a8d1bdc73e3e378a9f973e9cc54ac5d119
Sarah Marshall <sarah@sarahmarshall.name>
2021-06-05 21:52:59 -0700

    Increase bullet speed

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,7 +6,7 @@ namespace PinkRain
 {
     public class Gun : MonoBehaviour
     {
-        private const float BulletSpeed = 15;
+        private const float BulletSpeed = 30;
         private const int ClipSize = 16;
         private const float FireRate = 20;
         private const float ReloadTime = 0.5f;
